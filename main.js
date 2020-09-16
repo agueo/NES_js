870ebb7a8f735949c3ca8a8a2b7cc6a80bd907ff
Agueo <lopez.agueo@gmail.com>
2020-09-15 21:58:38 -0400

    Initial Commit
    Starting structure for Project

---
+++ main.js
@@ -0,0 +1,4 @@
+const Disassembler = require("./src/Core/Disassembler");
+
+console.log('We are starting up');
+var ds = new Disassembler();
---
+++ src/Core/Disassembler.js
@@ -0,0 +1,7 @@
+class Disassembler {
+    constructor() {
+        console.log("New Disassembler object")
+    }
+}
+
+module.exports = Disassembler;
