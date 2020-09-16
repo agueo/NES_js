4ab8700549dd485c2dab3a8b96758967984624e9
Agueo <lopez.agueo@gmail.com>
2020-09-16 14:28:57 -0400

    Create CPU class file

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const Disassembler = require("./src/Core/Disassembler");
+const CPU = require("./src/Core/Cpu");
+
 
 console.log('We are starting up');
 var ds = new Disassembler();
+var cpu = new CPU();
---
+++ src/Core/Cpu.js
@@ -0,0 +1,7 @@
+class CPU {
+    constructor() {
+        console.log("New CPU object");
+    }
+}
+
+module.exports = CPU;
